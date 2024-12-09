43c3172e7d7d90e1a1d281b22c63f5cefcd653cb
DiegoRMV <95139700+DiegoRMV@users.noreply.github.com>
2024-12-09 14:05:41 -0300

    update title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Title = ({ title="" }) => {
 	return (
-		<h2 className="font-oswald text-4xl md:text-start md:text-5xl font-extrabold inline border-b-4 bg-nuevo bg-clip-text text-transparent border-gray-600 dark:border-white dark:text-white">
+		<h2 className="font-oswald text-4xl md:text-start md:text-5xl font-extrabold inline border-b-4 bg-nuevo bg-clip-text border-gray-600 dark:border-white text-gray-900 dark:text-white">
 			{title}
 		</h2>
 	);
